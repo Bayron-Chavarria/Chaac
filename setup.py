95b8099f6687000223a99a945347aa34f43dcacc
Bayron-Chavarria <114596906+Bayron-Chavarria@users.noreply.github.com>
2023-06-22 17:40:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if os.name in ('nt', 'dos'):
 def main():
     setup(
         # Application name:
-        name="probar123",
+        name="works_colombia",
 
         # Version number (initial):
         version="0.0.2",
