232eb877b08c84a682aa33583a760c467d1358fe
Bayron-Chavarria <114596906+Bayron-Chavarria@users.noreply.github.com>
2023-06-22 15:27:57 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,8 +4,7 @@ from desoper import prueba
 
 class Test_hello(unittest.TestCase):
     def test__working(self):
-        self.assertEqual(prueba.hello(),
-                         'Hello, World!', True)
+        self.assertEqual(prueba.hello(),'Hello, World!', True)
 
 
 if __name__ == '__main__':
