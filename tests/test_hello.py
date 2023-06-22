2c2241df39996f13c988f38bb06286d5b440907c
Bayron-Chavarria <114596906+Bayron-Chavarria@users.noreply.github.com>
2023-06-22 15:24:47 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,10 +1,10 @@
 import unittest
-from desoper import hello
+from desoper import prueba
 
 
 class Test_hello(unittest.TestCase):
     def test__working(self):
-        self.assertEqual(hello.hello(),
+        self.assertEqual(prueba.hello(),
                          'Hello, World!', True)
 
 
