c1f4a2cf8be7fee00b19d83dd3c2a88cd10f98b0
Bayron-Chavarria <114596906+Bayron-Chavarria@users.noreply.github.com>
2023-07-13 15:14:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="Chaac",
 
         # Version number (initial):
-        version="0.0.3",
+        version="0.0.4",
 
         # Application author details:
         author="Bayron",
